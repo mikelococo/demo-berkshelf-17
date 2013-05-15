faa5c2ecfad48fd6951c890d9c76a078625b18d5
Mike Lococo <mikelococo@gmail.com>
2013-05-14 21:21:00 -0400

    Commit the rest of the files

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "demo_berkshelf_17"
+maintainer       "Mike Lococo"
+maintainer_email "mikelococo@gmail.com"
+license          "Public Domain"
+description      "Demonstrates berkshelf bug 17: https://github.com/RiotGames/vagrant-berkshelf/issues/17"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+depends "apt"
---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: mikelococo_com
+# Recipe:: default
+#
+# Copyright (C) 2013 YOUR_NAME
+# 
+# Public Domain
+#
+#
+
+# Updates the apt-cache, otherwise package installations may fail
+include_recipe "apt"
